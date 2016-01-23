c5e40a007d66176d13fa8417fba9c4a8ed983ff5
norbertkorodi <norbert.korodi@gmail.com>
2016-01-24 00:18:58 +0100

    Logger added to QuestionsApiLiveTest.java

--- src/test/java/org/stackexchange/api/client/QuestionsApiLiveTest.java
+++ src/test/java/org/stackexchange/api/client/QuestionsApiLiveTest.java
@@ -1,28 +1,26 @@
 package org.stackexchange.api.client;
 
-import static org.hamcrest.Matchers.containsString;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.greaterThan;
-import static org.hamcrest.Matchers.notNullValue;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertThat;
-
-import java.io.IOException;
-
-import org.apache.commons.lang3.StringEscapeUtils;
-import org.apache.http.HttpHeaders;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.impl.client.DecompressingHttpClient;
-import org.junit.Before;
-import org.junit.Test;
-import org.stackexchange.api.constants.StackSite;
-
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
+import org.apache.commons.lang3.StringEscapeUtils;
+import org.apache.http.HttpHeaders;
+import org.apache.http.HttpResponse;
+import org.apache.http.impl.client.DecompressingHttpClient;
+import org.junit.Before;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.stackexchange.api.constants.StackSite;
+
+import java.io.IOException;
+
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
 
 public class QuestionsApiLiveTest {
+    private final Logger logger = LoggerFactory.getLogger(getClass());
     private QuestionsApi questionsApi;
 
     // fixtures
@@ -37,47 +35,49 @@ public class QuestionsApiLiveTest {
     // serverfault
 
     @Test
-    public final void whenRequestIsPerformed_thenNoExceptions() throws ClientProtocolException, IOException {
+    public final void whenRequestIsPerformed_thenNoExceptions() throws IOException {
         questionsApi.questions(70, StackSite.ServerFault);
     }
 
     @Test
-    public final void whenRequestIsPerformed_thenSuccess() throws ClientProtocolException, IOException {
+    public final void whenRequestIsPerformed_thenSuccess() throws IOException {
         final HttpResponse httpResponse = questionsApi.questionsAsResponse(70, StackSite.ServerFault);
         assertThat(httpResponse.getStatusLine().getStatusCode(), equalTo(200));
     }
 
     @Test
-    public final void whenRequestIsPerformed_thenOutputIsJson() throws ClientProtocolException, IOException {
+    public final void whenRequestIsPerformed_thenOutputIsJson() throws IOException {
         final HttpResponse httpResponse = questionsApi.questionsAsResponse(70, StackSite.ServerFault);
         final String contentType = httpResponse.getHeaders(HttpHeaders.CONTENT_TYPE)[0].getValue();
         assertThat(contentType, containsString("application/json"));
     }
 
     @Test
-    public final void whenRequestIsPerformed_thenOutputIsCorrect() throws ClientProtocolException, IOException {
+    public final void whenRequestIsPerformed_thenOutputIsCorrect() throws IOException {
         final String responseBody = questionsApi.questions(70, StackSite.ServerFault);
+        logger.debug(responseBody);
         assertThat(responseBody, notNullValue());
     }
 
     @Test
-    public final void whenParsingOutputFromQuestionsApi_thenOutputContainsSomeQuestions() throws ClientProtocolException, IOException {
+    public final void whenParsingOutputFromQuestionsApi_thenOutputContainsSomeQuestions() throws IOException {
         final String questionsAsJson = questionsApi.questions(70, StackSite.ServerFault);
         final ObjectMapper mapper = new ObjectMapper();
         final JsonNode rootNode = mapper.readTree(questionsAsJson);
         final ArrayNode questionsArray = (ArrayNode) rootNode.get("items");
+        logger.debug(questionsArray.toString());
         assertThat(questionsArray.size(), greaterThan(20));
     }
 
     // askubuntu
 
     @Test
-    public final void givenOnAskUbuntu_whenInitialRequestIsPerformed_thenNoExceptions() throws ClientProtocolException, IOException {
+    public final void givenOnAskUbuntu_whenInitialRequestIsPerformed_thenNoExceptions() throws IOException {
         questionsApi.questions(100, StackSite.AskUbuntu);
     }
 
     @Test
-    public final void givenOnAskUbuntu_whenParsingOutputFromQuestionsApi_thenOutputIsParsable() throws ClientProtocolException, IOException {
+    public final void givenOnAskUbuntu_whenParsingOutputFromQuestionsApi_thenOutputIsParsable() throws IOException {
         final String questionsAsJson = questionsApi.questions(70, StackSite.AskUbuntu);
         final ObjectMapper mapper = new ObjectMapper();
         final JsonNode rootNode = mapper.readTree(questionsAsJson);
@@ -88,7 +88,7 @@ public class QuestionsApiLiveTest {
     // character encoding
 
     @Test
-    public final void givenOutputFromQuestionsApi_whenCharacterEncodingIsAnalyzed_thenOutputIsParsable() throws ClientProtocolException, IOException {
+    public final void givenOutputFromQuestionsApi_whenCharacterEncodingIsAnalyzed_thenOutputIsParsable() throws IOException {
         final String questionsAsJson = questionsApi.questions(70, StackSite.AskUbuntu);
         final ObjectMapper mapper = new ObjectMapper();
         final JsonNode rootNode = mapper.readTree(questionsAsJson);
@@ -104,7 +104,7 @@ public class QuestionsApiLiveTest {
     // stackoverflow tag
 
     @Test
-    public final void givenOnSO_whenRequestOnTagIsPerformed_thenNoExceptions() throws ClientProtocolException, IOException {
+    public final void givenOnSO_whenRequestOnTagIsPerformed_thenNoExceptions() throws IOException {
         questionsApi.questions(100, StackSite.AskUbuntu);
     }
 
