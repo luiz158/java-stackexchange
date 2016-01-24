9d534da38c45457aade0e1c6ccf12096f3ce3d65
norbertkorodi <norbert.korodi@gmail.com>
2016-01-24 20:05:33 +0100

    buildURIString created for refactor

--- src/main/java/org/stackexchange/api/client/ApiUris.java
+++ src/main/java/org/stackexchange/api/client/ApiUris.java
@@ -15,6 +15,7 @@ import java.util.stream.Stream;
 public class ApiUris {
     private static final String API_2_1 = "https://api.stackexchange.com/2.1";
     private static final String API_2_2 = "https://api.stackexchange.com/2.2";
+//    private static final String SAMPLE_EXCERPT_SEARCH= "https://api.stackexchange.com/2.2/search/excerpts?page=StackOverflow&pagesize=10&fromdate=1451952000&todate=1451952000&order=desc&min=1451779200&max=1451779200&sort=activity&q=python&accepted=True&answers=yes&body=asdf&closed=False&migrated=True&notice=True&nottagged=asdf&tagged=tagged&title=title&user=asdf&url=asdf&views=views&wiki=True&site=stackoverflow";
 
     private static final Logger logger = LoggerFactory.getLogger(ApiUris.class);
 
@@ -45,7 +46,6 @@ public class ApiUris {
         return buildQuestionsMultipleUriParams(min, site, Questions.S_TAGS_S + tag + Questions.S_FAQ, page);
     }
 
-    // util
     static String buildQuestionsMultipleUriParams(final int min, final StackSite site, final String operation, final int page) {
         Map<String, ? extends Serializable> uriParams = Collections.unmodifiableMap(Stream.of(
                 new SimpleEntry<>(Questions.ORDER, Questions.DESC),
@@ -54,23 +54,27 @@ public class ApiUris {
                 new SimpleEntry<>(Questions.SITE, site),
                 new SimpleEntry<>(Questions.PAGE, page))
                 .collect(Collectors.toMap(SimpleEntry::getKey, SimpleEntry::getValue)));
-        final String params = getBuild(min, site, page, uriParams);
+        return buildURIString(operation, uriParams);
+    }
+
+    private static String buildURIString(String operation, Map<String, ? extends Serializable> uriParams) {
+        final String params = buildParamsString(uriParams);
         return getApiString() + operation + params;
     }
 
-    private static String getBuild(int min, StackSite site, int page, Map<String, ? extends Serializable> uriParamKeyValue) {
+    private static String buildParamsString(Map<String, ? extends Serializable> uriParamKeyValue) {
         RequestBuilder requestBuilder = new RequestBuilder();
         uriParamKeyValue.entrySet().stream().forEach(e -> requestBuilder.add(e.getKey(), e.getValue()));
         return requestBuilder.build();
     }
 
+    private static String getApiString() {
+        return API_2_2;
+    }
+
     static String getSingleUri(final StackSite site, final String operation) {
         final String params = new RequestBuilder().add(Questions.SITE, site).build();
         return getApiString() + operation + params;
-    }
-
-    private static String getApiString() {
-        return API_2_2;
     }
 
 }
