e2dea84d2c9c9131d6f4d143e9cf80e8fa4c6ee5
norbertkorodi <norbert.korodi@gmail.com>
2016-01-24 16:35:51 +0100

    Small refactor + 2.2 stackexchange api is gonna be used.

--- src/main/java/org/stackexchange/api/client/ApiUris.java
+++ src/main/java/org/stackexchange/api/client/ApiUris.java
@@ -1,11 +1,15 @@
 package org.stackexchange.api.client;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.stackexchange.api.constants.ApiConstants.Questions;
 import org.stackexchange.api.constants.StackSite;
 
 public class ApiUris {
     private static final String API_2_1 = "https://api.stackexchange.com/2.1";
     private static final String API_2_2 = "https://api.stackexchange.com/2.2";
+
+    private static final Logger logger = LoggerFactory.getLogger(ApiUris.class);
 
     private ApiUris() {
         throw new AssertionError();
@@ -18,7 +22,7 @@ public class ApiUris {
     }
 
     public static String getQuestionsUri(final int min, final StackSite site, final int page) {
-        return getMultipleUri(min, site, Questions.S_QUESTIONS, page);
+        return buildQuestionsMultipleUriParams(min, site, Questions.S_QUESTIONS, page);
     }
 
     public static String getSingleQuestionUri(final StackSite site, final long id) {
@@ -31,22 +35,22 @@ public class ApiUris {
     }
 
     public static String getTagUri(final int min, final StackSite site, final String tag, final int page) {
-        return getMultipleUri(min, site, Questions.S_TAGS_S + tag + Questions.S_FAQ, page);
+        return buildQuestionsMultipleUriParams(min, site, Questions.S_TAGS_S + tag + Questions.S_FAQ, page);
     }
 
     // util
-    static String getMultipleUri(final int min, final StackSite site, final String operation, final int page) {
-        final String params = new RequestBuilder().add(Questions.order, Questions.DESC).add(Questions.sort, Questions.VOTES).add(Questions.min, min).add(Questions.site, site).add(Questions.page, page).build();
+    static String buildQuestionsMultipleUriParams(final int min, final StackSite site, final String operation, final int page) {
+        final String params = new RequestBuilder().add(Questions.ORDER, Questions.DESC).add(Questions.SORT, Questions.VOTES).add(Questions.MIN, min).add(Questions.SITE, site).add(Questions.PAGE, page).build();
         return getApiString() + operation + params;
     }
 
     static String getSingleUri(final StackSite site, final String operation) {
-        final String params = new RequestBuilder().add(Questions.site, site).build();
+        final String params = new RequestBuilder().add(Questions.SITE, site).build();
         return getApiString() + operation + params;
     }
 
     private static String getApiString() {
-        return API_2_1;
+        return API_2_2;
     }
 
 }
